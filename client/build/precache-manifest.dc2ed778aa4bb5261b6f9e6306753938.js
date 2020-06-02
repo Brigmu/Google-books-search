b6f8072d5a94b168d371d0f875ed161b0ffad8d2
Brigham Mueller <brigmu@gmail.com>
2020-06-02 14:36:33 -0700

    added cors and ran build

---
+++ client/build/precache-manifest.dc2ed778aa4bb5261b6f9e6306753938.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "654e45f67cfb410a0815",
+    "url": "/static/css/main.3fcfcc8a.chunk.css"
+  },
+  {
+    "revision": "654e45f67cfb410a0815",
+    "url": "/static/js/main.59e34d02.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "ae8d3f9023f5629f982a",
+    "url": "/static/js/2.6e8b6ae0.chunk.js"
+  },
+  {
+    "revision": "306dac2c35d409a9dc46bd5f8d4d6b7a",
+    "url": "/index.html"
+  }
+];
--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const PORT = process.env.PORT || 3001;
 const app = express();
 const routes = require("./routes");
 const mongoose = require('mongoose');
+const cors = require('cors');
 
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
@@ -12,6 +13,9 @@ app.use(express.json());
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
+
+app.use(express.static(__dirname + "/client/public"));
+app.use(cors());
 
 // Connect to the Mongo DB
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/googlebooks");
